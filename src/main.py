64a0ad0969505fd5780374629e08237f381545a4
Trashcorp567 <Trashcorp567@gmail.com>
2024-04-23 18:51:26 +0300

    Update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     max_pages = int(input("Количество выводимых страниц: "))
     parser = MetroPars(max_pages)
     parser.parse()
-    parser.save_to_json("products.json")
-    parser.save_to_csv("products.csv")
+    parser.save_to_json("src/products.json")
+    parser.save_to_csv("src/products.csv")
     print("\nОбъекты загружены и сохранены.")
